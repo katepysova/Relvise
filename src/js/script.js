0bc393ac78e4596c8c81bd945bad9694ec4885f0
Kateryna <katepysova97@gmail.com>
2022-12-02 22:01:42 +0100

    Added js page navigation

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,37 @@ const navBtn = document.querySelector(".nav__btn");
 const navElem = document.querySelector(".nav");
 const htmlEl = document.querySelector("html");
 
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else if (href !== "#" && href.startsWith("#")) {
+      const target = document.querySelector(href);
+      target.scrollIntoView({
+        behavior: "smooth",
+        top: 40,
+      });
+    }
+
+    if (navBtn.classList.contains("open")) {
+      navBtn.classList.remove("open");
+    }
+    if (navElem.classList.contains("open")) {
+      navElem.classList.remove("open");
+    }
+    if (htmlEl.classList.contains("u-static")) {
+      htmlEl.classList.remove("u-static");
+    }
+  });
+});
+
 navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("open");
   navElem.classList.toggle("open");
